7cc2624fbc5e3a68ad89e7bb034db05a5e46e2c8
paul <paultk@stud.ntnu.no>
2017-09-12 16:41:01 +0200

    Initial commit

---
+++ App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import App from './App';
+
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('should be true', () => {
+  expect(true).toBeTruthy();
+});
+
+it('', () => {
+  let app = new App();
+
+  expect(app.func1()).toBeTruthy();
+});
