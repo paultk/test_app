9687287a29d0b6fd806c741b53e8d09b47eb6637
paul <paultk@stud.ntnu.no>
2017-09-13 11:33:41 +0200

    Trying the renderer test

--- App.test.js
+++ App.test.js
@@ -3,10 +3,10 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
-// it('renders without crashing', () => {
-//   const rendered = renderer.create(<App />).toJSON();
-//   expect(rendered).toBeTruthy();
-// });
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
 
 it('should be true', () => {
   expect(true).toBeTruthy();
